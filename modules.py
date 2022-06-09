7ed64bb5c93686538e7393c42268ad5727f21a2e
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-09 12:17:38 +0530

    datetime and random modules

---
+++ modules.py
@@ -0,0 +1,24 @@
+from random import *
+from datetime import datetime
+
+# def die(num):
+#     seed = num
+#     return randint(1,6)
+
+# print(die(3))
+
+
+
+# lst = ['a','b','c','d']
+# shuffle(lst)
+# print(lst)
+
+# lst = ['a','b','c','d']
+# print(choice(lst))
+
+Time = datetime.now()
+date = datetime.today()
+print(Time)
+print(date)
+
+print("this year is", date.year)
