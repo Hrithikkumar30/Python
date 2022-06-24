cd74b50114520d0378a434d120576a9e156ec620
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-24 12:58:15 +0530

    multiple data ploting in one chart

--- Data Sciencee/DataAnalysis&Visualization/matplotlibVisualize.py
+++ Data Sciencee/DataAnalysis&Visualization/matplotlibVisualize.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
 
 
 data = pd.read_csv("C:/Users\hrith\Downloads/csv_data.csv" , 
@@ -37,3 +38,74 @@ dt = {
 sales = pd.DataFrame(data=dt, index=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
 
 print(sales)
+
+
+
+
+
+# creating multiple data into 1 graph
+
+
+#3 data plot in one chart
+
+  
+N = 3
+ind = np.arange(N) 
+width = 0.25
+  
+xvals = [8, 9, 2]
+bar1 = plt.bar(ind, xvals, width, color = 'r')
+  
+yvals = [10, 20, 30]
+bar2 = plt.bar(ind+width, yvals, width, color='g')
+  
+zvals = [11, 12, 13]
+bar3 = plt.bar(ind+width*2, zvals, width, color = 'b')
+  
+plt.xlabel("Dates")
+plt.ylabel('Scores')
+plt.title("Players Score")
+  
+plt.xticks(ind+width,['2021Feb01', '2021Feb02', '2021Feb03'])
+plt.legend( (bar1, bar2, bar3), ('Player1', 'Player2', 'Player3') )
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+#2data plot in one  chart
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+  
+X = ['Group A','Group B','Group C','Group D']
+Ygirls = [10,20,20,40]
+Zboys = [20,30,25,30]
+  
+X_axis = np.arange(len(X))
+  
+plt.bar(X_axis - 0.2, Ygirls, 0.4, label = 'Girls')
+plt.bar(X_axis + 0.2, Zboys, 0.4, label = 'Boys')
+  
+plt.xticks(X_axis, X)
+plt.xlabel("Groups")
+plt.ylabel("Number of Students")
+plt.title("Number of Students in each group")
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+# 
