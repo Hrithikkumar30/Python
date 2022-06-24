555fec4fa066c7c41109d26d8dda8554df4d90e6
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-24 12:54:53 +0530

    pandas dataframe into char using plotlib

--- Data Sciencee/pandas.py
+++ Data Sciencee/pandas.py
@@ -32,16 +32,41 @@ import numpy as np
 
 
 
-THreeDarr = []
+import matplotlib.pyplot as plt
+  
+# creating dataframe
+df = pd.DataFrame({
+    'Name': ['John', 'Sammy', 'Joe'],
+    'Age': [45, 38, 90],
+    'Height(in cm)': [150, 180, 160]
+})
+  
+# creating subplots and plotting them together
+ax = plt.subplot()
+ax.bar(df["Name"], df["Height(in cm)"])
+ax.bar(df["Name"], df["Age"], color="maroon")
 
-for i in range (1,4):
-    twodAr = []
-    for j in range (1,4):
-        twodAr.append([i,j])
-    THreeDarr.append(twodAr)
-
-finalArr = np.array(THreeDarr)
-print(finalArr)
 
 
-df = pd.DataFrame(data=finalArr )  
+  
+# creating dataframe
+df = pd.DataFrame({
+    'Name': ['John', 'Sammy', 'Joe'],
+    'Age': [45, 38, 90],
+    'Height(in cm)': [150, 180, 160]
+})
+  
+# plotting Height
+ax = df.plot(x="Name", y="Height(in cm)", kind="bar")
+# plotting age on the same axis
+df.plot(x="Name", y="Age", kind="bar", ax=ax, color="maroon")
+
+
+df = pd.DataFrame({
+    'Name': ['John', 'Sammy', 'Joe'],
+    'Age': [45, 38, 90],
+    'Height(in cm)': [150, 180, 160]
+})
+  
+# plotting graph
+df.plot(x="Name", y=["Age", "Height(in cm)"], kind="bar")
